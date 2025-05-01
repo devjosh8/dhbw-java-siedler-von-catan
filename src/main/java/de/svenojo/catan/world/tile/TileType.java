78ef10f2d9c1a536b15db273f8ed1f8590656485
Joshua Kandel <joshua.8.kandel@gmail.com>
2025-05-01 16:30:56 +0200

    feat: add farmland and grassland; overhaul woods, desert and mountain models

--- src/main/java/de/svenojo/catan/world/tile/TileType.java
+++ src/main/java/de/svenojo/catan/world/tile/TileType.java
@@ -4,8 +4,9 @@ public enum TileType {
     
     WOODS("data/models/karte_wald.g3db"),
     DESERT("data/models/karte_wueste.g3db"),
-    MOUNTAINS("data/models/karte_berge.g3db");
-
+    MOUNTAINS("data/models/karte_berge.g3db"),
+    FARMLAND("data/models/karte_farmland.g3db"),
+    GRASSLAND("data/models/karte_weideland.g3db");
 
     private String fileName;
     TileType(String fileName) {

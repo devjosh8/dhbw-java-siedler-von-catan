6ff16a1372a65ea899f420d0f31527193266d410
Joshua Kandel <joshua.8.kandel@gmail.com>
2025-07-13 12:54:06 +0200

    fix: allow 11 and 12 to be rolled

--- src/main/java/de/svenojo/catan/logic/DiceRoll.java
+++ src/main/java/de/svenojo/catan/logic/DiceRoll.java
@@ -6,7 +6,7 @@ public class DiceRoll {
     private Random random = new Random();
 
     private int singleDiceRoll() {
-        return this.random.nextInt(5) + 1; 
+        return this.random.nextInt(6) + 1; 
     }
     public int rollBothDice() {
         int firstDice = singleDiceRoll();
